f455bb2fe97b8ecbacec89615c2e8642506baebf
robbertzhou <1243774355@qq.com>
2020-04-04 15:01:14 +0800

    sa
    xzzx

---
+++ src/java/com/zy/FirstApplication.java
@@ -0,0 +1,9 @@
+package com.zy;
+
+//@SpringBootApplication
+public class FirstApplication {
+    public static void main(String[] args){
+//        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("hello world");
+    }
+}
